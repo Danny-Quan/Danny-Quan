5b503256c5b15717ce392a00ea11cfc35f7f478f
Danny-Quan <71638775+Danny-Quan@users.noreply.github.com>
2021-07-23 18:36:06 +0100

    Add files via upload

---
+++ governance_overview.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Youth Empowerment Consortium</title>
+    <!-- include boostrap css style -->
+    <link href="vendors/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/jpg" href="resources/css/img/logo.jpeg">
+</head>
+<body>
+
+<?php
+include_once("header.php");
+?>
+
+<section class="ministry_section">
+    <div class="ministry_header">
+        <h1 class="text-uppercase text-center">overview on governance and political leadership</h1>
+    </div>
+    <div class="container">
+        <p>
+        <h3 class="text-uppercase text-center">overview</h3>         
+    <p>
+    Within our Governance program  are  youth leaders, Community Development Agents, Ministers, Ambassadors and Entrepreneurs of age 18-40 who represent their communities, Districts, Constituencies , NGOs and various ministries of interest. They move motions for debates and highlight the concerns and challenges of young people of issues such as Youth Employment, National development policies, Political party Manefestos , state of the economy,Youth contribution to Economic growth, Challenges in their communities and design alternative solutions which they implement as their projects.<br><br>
+.This is about leadership development, policy and not Politics. <br><br>
+
+They Are therefore able to examine leadership paradigms and their consequences, they examine different models of Nation building principles and from that understands what Africa can do to develop in this season.<br><br>
+
+Our objective is to prepare future Presidents, Ministers of State, Ambassadors, Members of parliament and Diplomats today for transformational leadership. These will be national development champions who will be known for their Responsibilities in Action, Integrity In character , Compassion in outlook and Excellence national development projects Execution. <br>
+
+    </p>
+    </p>
+    </div>
+</section>
+
+<?php
+include_once("footer.php");
+?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://kit.fontawesome.com/011545c454.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="vendors/js/bootstrap.min.js"></script>
+</body>
+</html>
