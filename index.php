ba675484bf908289d083df376b07129a3743d3e0
Danny-Quan <71638775+Danny-Quan@users.noreply.github.com>
2021-07-23 23:39:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -427,6 +427,12 @@ Our objective is to prepare future Presidents, Ministers of State, Ambassadors, 
     </div>
 
     </section>
+
+    <section class="facebook_feed">
+<div class=" container f_feed">
+<iframe style="width:100%; height:50vh;" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fyecglobal%2F&tabs=timeline&width=340&height=271&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="271" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+</div>
+    </section>
     <!-- section 7 -->
 <div class="whatsApp_btn">
   <a href="https://wa.me/message/WVRMA5IBPIQND1" target="_blank"><i class="fab fa-whatsapp"></i></a>
